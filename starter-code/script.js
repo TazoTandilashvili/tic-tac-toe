24704e624cf2441e05a7334c6d2af91ec0d1b696
Tazo <tazo.tandilashvili2004@gmail.com>
2024-05-22 18:40:07 +0400

    reupload code

---
+++ starter-code/script.js
@@ -0,0 +1,16 @@
+'use strict'
+const choiceButtons = document.querySelectorAll('.btn-box');
+
+let player1 = 'x';
+
+const activateChoice = (icon) => {
+  if (icon === 'x') {
+    choiceButtons[0].classList.add('active');
+    choiceButtons[1].classList.remove('active');
+    player1 = 'x';
+  } else {
+    choiceButtons[0].classList.remove('active');
+    choiceButtons[1].classList.add('active');
+    player1 = 'o';
+  }
+}
